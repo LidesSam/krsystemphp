2112d6cb54b1950df40948e136e7e1b4bf13f588
LidesSam <lides.sam@gmail.com>
2022-07-19 16:50:38 -0300

    testview

--- index.php
+++ index.php
@@ -5,14 +5,27 @@
     <title>
         kr system monitor
     </title>
+    <link rel="stylesheet" href="./style.css"/>
 </head>
 
 <body>
  <?php 
     include_once("php\masterConection.php");
-    PostgressConnection::ConnectDB();
+    $conector = new PostgressConnection;
+    $conector->ConnectDB();
+    $conector->showBotProfile();
+    $conector->displayAsTable("drivers");
+  
+   
+
+    
 
  ?>
+
+ <div id="cssexample" class="botprofile">
+    name: cssexample
+    <div class="colorblock"></div>
+</div>
 </body>
 
 </html>
--- php/masterConection.php
+++ php/masterConection.php
@@ -1,19 +1,68 @@
 <?php
 class PostgressConnection
 {
-    static function ConnectDB()
+
+    public $conn = null;
+    public $name = "def";
+
+    function __construct()
     {
+    }
+
+    public function ConnectDB()
+    {
+
         $host = 'localhost';
         $db = 'krsystem';
         $user = 'postgres';
         $password = 'Arrieta27'; // change to your password
-        try{    
-            $conn = new Pdo("pgsql:host=$host;dbname=$db",$user,$password,);
+        try {
+            $this->conn = new Pdo("pgsql:host=$host;dbname=$db", $user, $password,);
             echo "connected to table:$db";
-            
+        } catch (PDOException $exp_error) {
+            echo ("failed connection error:$exp_error");
         }
-        catch(PDOException $exp_error){
-            echo("failed connection error:$exp_error");
+    }
+
+
+    public function showBotProfile()
+    {
+        $sql = "Select * From drivers order by id asc";
+        echo '<h1> "bot profile" </h2>';
+
+
+        try {
+            echo '<div id="botdisp" class="botdisplay">';
+            foreach ($this->conn->query($sql) as $row) {
+                $colorstr ="";
+
+                echo "<div  id=".$row['id'].' class="botprofile">name:'.$row["name"].' cssexample
+                     <h3 class="botname"> Bot:'.$row['name'].' </h3><hr/>
+                    <div class="colorblock" style="background-color:'.$row["colorcode"].';"></div>
+                    <div class="state">'. $row["state"].'</div>
+                </div>';
+                
+            }
+            
+            echo "</div>";
+        } catch (PDOException $exp_error) {
+            echo ("failed connection error:$exp_error");
+        }
+    }
+
+    //display the content of a query in a table on
+    public function displayAsTable($tablename = "")
+    {
+        $sql = "Select * From $tablename";
+        echo '<h1> "btable " </h1>';
+        $result=$this->conn->query($sql);
+        try {
+            echo gettype($this->conn->query($sql))."<br>";
+            foreach ($this->conn->query($sql) as $row) {
+                
+            }
+        } catch (PDOException $exp_error) {
+            echo ("failed connection error:$exp_error");
         }
     }
 }
